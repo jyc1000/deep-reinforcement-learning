a27a3876576ffeb85325d29d587615546e73b847
junyul <jyc1000@gmail.com>
2023-07-03 22:21:07 +0900

    basic implementation of  taxi problem

--- lab-taxi/main.py
+++ lab-taxi/main.py
@@ -1,8 +1,8 @@
 from agent import Agent
 from monitor import interact
-import gym
+import gymnasium as gym
 import numpy as np
 
-env = gym.make('Taxi-v2')
+env = gym.make('Taxi-v3')
 agent = Agent()
 avg_rewards, best_avg_reward = interact(env, agent)
